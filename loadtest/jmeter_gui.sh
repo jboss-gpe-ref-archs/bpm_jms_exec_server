aa32ee60ed7baaf75c33914f8cf792fd2852a61b
ratwater <jbride2001@yahoo.com>
2014-07-09 10:57:02 -0600

    added few comments to jmeter_gui.sh

--- loadtest/jmeter_gui.sh
+++ loadtest/jmeter_gui.sh
@@ -1,6 +1,8 @@
 
 # Purpose
 #  - start up the JMeter gui 
-#  - uses the exact JMeter run-time and dependencies that will be used during execution of a test driven my jmeter-maven-plugin
+#  - uses the exact JMeter run-time and dependencies that will be used during execution of a test driven by jmeter-maven-plugin
+#
+# NOTE:  specify '-n' to execute the test harness in non-gui mode
 
 java -Xms512M -Xmx512M -jar target/jmeter/bin/ApacheJMeter.jar -l target/jmeter/results/ref_arch.jtl -d target/jmeter -j target/jmeter/logs/ref_arch.jmx.log -t src/test/jmeter/ref_arch.jmx
